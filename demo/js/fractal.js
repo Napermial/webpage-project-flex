ee3bec3a357c5c33d1e2eecb2018ce8a955c8ccf
Napermial <a>
2019-12-06 09:44:32 +0100

    fractal generates

--- demo/js/fractal.js
+++ demo/js/fractal.js
@@ -1,17 +1,17 @@
 
 const myCanvas = document.querySelector('.treeHolder');
-let context = myCanvas.getContext();
+let context = myCanvas.getContext("2d");
 
-const magnificationFactor = 600;
-let panX = 0;
-let panY = 0;
+const magnificationFactor = 200;
+let panX = 2;
+let panY = 1.5;
 for(let x=0; x < myCanvas.width; x++) {
     for(let y=0; y < myCanvas.height; y++) {
         let belongsToSet =
             checkIfBelongsToMandelbrotSet(x / magnificationFactor - panX,
                 y / magnificationFactor - panY);
         if(belongsToSet) {
-            context.fillRect(x,y, 1,1); // Draw a black pixel
+            context.fillRect(x,y, 1,1);
         }
     }
 }
